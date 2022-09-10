fac14309e3b6b1a7023d3860cea932117e0338b1
minaaaatef <mina.atef0@gmail.com>
2022-09-11 01:02:12 +0200

    Change App Name

--- django_admin_buttons/boot_django.py
+++ django_admin_buttons/boot_django.py
@@ -6,13 +6,13 @@ import os
 import django
 from django.conf import settings
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "django_admin_button"))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "django_admin_buttons"))
 
 def boot_django():
     settings.configure(
         BASE_DIR=BASE_DIR,
         INSTALLED_APPS=(
-            "django_admin_button",
+            "django_admin_buttons",
         ),
         TIME_ZONE="UTC",
         USE_TZ=True,
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from glob import glob
 
 
 setup(
-    name='django_admin_button',
-    version='0.2.0',
+    name='django_admin_buttons',
+    version='0.2.1',
     license='MIT',
     author="Mina Atef",
     author_email='mina.atef0@gmail.com',
-    packages=['django_admin_button','django_admin_button.templates','django_admin_button.static'],
+    packages=['django_admin_buttons','django_admin_buttons.templates','django_admin_buttons.static'],
         data_files = [
         ('templates', glob('templates/*')), # files in source_dir only - not recursive
         ('static', glob('static/*')), # includes sub-folders - recursive
